1410f1a6bd4cd5eea1b366d6e094b0ed896f946d
amindid <aouchaad@c1r6p1.1337.ma>
2023-10-18 16:15:30 +0100

    cpp02 is done

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aouchaad <aouchaad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:46:04 by aouchaad          #+#    #+#             */
-/*   Updated: 2023/10/16 16:52:07 by aouchaad         ###   ########.fr       */
+/*   Updated: 2023/10/18 15:51:40 by aouchaad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main() {
 	Point a(2.0f, 2.0f);
 	Point b(7.0f, 2.0f);
 	Point c(5.0f, 8.0f);
-	Point point(7.0f,2.0f);
+	Point point(4.0f,2.1f);
 	if (bsp(a, b, c, point))
 		std::cout << "the point is inside the triangle" << std::endl;
 	else
